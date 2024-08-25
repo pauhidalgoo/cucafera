e61e02d3674810fdcccf5d9b61daec189627f0fa
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-08-25 23:15:39 +0200

    ADD comments to match hf state dict

--- src/model.py
+++ src/model.py
@@ -28,8 +28,8 @@ class CausalSelfAttentionGQA(nn.Module):
 
         shape = (config.n_head + 2 * config.n_kv_heads) * self.head_dim
 
-        self.c_attn = nn.Linear(config.n_embd, shape, bias=False)
-        self.c_proj = nn.Linear(self.head_dim * config.n_head, config.n_embd, bias=False)
+        self.c_attn = nn.Linear(config.n_embd, shape, bias=False) # q_proj, k_proj i v_proj joined
+        self.o_proj = nn.Linear(self.head_dim * config.n_head, config.n_embd, bias=False) # o_proj
         
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -86,7 +86,7 @@ class CausalSelfAttentionGQA(nn.Module):
             y = torch.matmul(scores, v)
 
         y = y.transpose(1, 2).contiguous().view(B, T, C)
-        y = self.c_proj(y)
+        y = self.o_proj(y)
         return y
 
 
@@ -94,7 +94,7 @@ class RMSNorm(nn.Module):
     def __init__(self, d, eps=1e-5):
         super().__init__()
         self.eps = eps
-        self.scale = nn.Parameter(torch.ones(d))
+        self.scale = nn.Parameter(torch.ones(d)) # weight
 
     def forward(self, x):
         norm = torch.sqrt(x.float().pow(2).mean(-1, keepdim=True) + self.eps).type_as(x)
@@ -187,9 +187,9 @@ class MLP(nn.Module):
     """
     def __init__(self, config):
         super().__init__()
-        self.w1   = nn.Linear(config.n_embd, config.intermediate_size, bias=False)
-        self.w2 = nn.Linear(config.intermediate_size, config.n_embd, bias=False)
-        self.w3 = nn.Linear(config.n_embd, config.intermediate_size, bias=False)
+        self.w1   = nn.Linear(config.n_embd, config.intermediate_size, bias=False) # gate_proj
+        self.w2 = nn.Linear(config.intermediate_size, config.n_embd, bias=False) # down proj
+        self.w3 = nn.Linear(config.n_embd, config.intermediate_size, bias=False) # up proj
     
     def forward(self, x):
         return self.w2(F.gelu(self.w1(x), approximate="tanh")* self.w3(x)) 
@@ -199,9 +199,9 @@ class Block(nn.Module):
 
     def __init__(self, type, config):
         super().__init__()
-        self.norm_1 = RMSNorm(config.n_embd, config.norm_eps)
+        self.norm_1 = RMSNorm(config.n_embd, config.norm_eps) # input_layernorm
         self.attn = CausalSelfAttentionGQA(type, config)
-        self.norm_2 = RMSNorm(config.n_embd, config.norm_eps)
+        self.norm_2 = RMSNorm(config.n_embd, config.norm_eps) # post_attention_layernorm
         self.mlp = MLP(config)
     
     def forward(self, x, freq):
@@ -209,7 +209,7 @@ class Block(nn.Module):
         x = x + self.mlp(self.norm_2(x))
         return x
 
-class GPT(nn.Module):
+class Aloja(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -217,7 +217,7 @@ class GPT(nn.Module):
         types = ["Local", "Normal"] * (config.n_layer // 2)
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wte = nn.Embedding(config.vocab_size, config.n_embd), #embed_tokens.weight
             h = nn.ModuleList([Block(types[i], config) for i in range(config.n_layer)]),
             norm_f =  RMSNorm(config.n_embd, config.norm_eps),
         ))
@@ -232,8 +232,6 @@ class GPT(nn.Module):
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
             std = 0.02
-            if hasattr(module, "NANOGPT_SCALE_INIT"):
-                std *= (2 * self.config.n_layer) ** -0.5
             torch.nn.init.normal_(module.weight, mean = 0.0, std=std)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
@@ -256,48 +254,7 @@ class GPT(nn.Module):
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
-        
-
-    @classmethod
-    def from_pretrained(cls, model_type):
-        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        from transformers import GPT2LMHeadModel
-        print("loading weights from pretrained gpt: %s" % model_type)
-
-        config_args = {
-            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),
-            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024),
-            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280),
-            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600),
-        }[model_type]
-        config_args['vocab_size'] = 50257
-        config_args['block_size'] = 1024
-
-        config = GPTConfig(**config_args)
-        model = GPT(config)
-        sd = model.state_dict()
-        sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
-
-        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-        sd_hf = model_hf.state_dict()
-
-        sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-
-        assert len(sd_keys_hf) == len(sd_keys), f"missmatches_keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-        for k in sd_keys_hf:
-            if any(k.endswith(w) for w in transposed):
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
-        return model
+    
     
     def configure_optimizers(self, learning_rate, weight_decay=0.0, betas=(0.9, 0.97), device_type='cuda'):
         train_params = []
@@ -341,7 +298,7 @@ class GPT(nn.Module):
 
 
 @dataclass
-class GPTConfig:
+class AlojaConfig:
     block_size: int = 2048
     vocab_size: int = 50257
     n_layer: int = 32
