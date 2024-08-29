7adcbead83180d9bb38c33c7ac4dad52a4ba11f6
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-08-29 16:53:54 +0200

    MOD tokenizer

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,28 +1,15 @@
-from tokenizers.pre_tokenizers import Whitespace
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers import Tokenizer, models, pre_tokenizers, decoders, trainers, processors
+from tokenizers import ByteLevelBPETokenizer
 
-
-tokenizer = Tokenizer(BPE(unk_token="<unk>"))
-tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=True)
-
-
-
-tokenizer.decoder = decoders.ByteLevel()
-tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+tokenizer = ByteLevelBPETokenizer()
 
 
 special_tokens = ["<s>", "</s>", "<unk>", "<pad>", "<im_start>", "<im_end>", "<fim_prefix>", "<fim_middle>", "<fim_suffix>", "<mask>"]
-trainer = BpeTrainer(vocab_size=65536, show_progress = True, special_tokens=special_tokens)
- 
-tokenizer.pre_tokenizer = Whitespace()
+
 
 files = ["./data/catalan_text.txt"]
 
-tokenizer.train(files, trainer)
+tokenizer.train(files=files, vocab_size=500, show_progress = True, special_tokens=special_tokens)
 
 
-tokenizer.save("./tokenizer/byte-level-bpe.tokenizer.json")
+tokenizer.save("./tokenizer/mini.tokenizer.json")
 tokenizer.model.save("tokenizer")
