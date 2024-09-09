eba4d7578e28d14630db8cd4e6622273143b7615
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-09-09 23:45:54 +0200

    FIX hellaswag

--- eval/hellaswag.py
+++ eval/hellaswag.py
@@ -1,34 +1,10 @@
 """
-Copiat de nanoGPT
+NOTE: DOWNLOADING THE JSONL FROM HUGGINGFACE DIDN'T WORK, SO TO YOU NEED TO DOWNLOAD THE FILE MANUALLY AND ADD IT TO THE HELLASWAG FOLDER
+
+Copiat de nanoGPT, by Andrej Karpathy
 
 Vídeo:
 https://www.youtube.com/watch?v=l8pRSuU81PU&ab_channel=AndrejKarpathy
-
-
-Downloads and evaluates HellaSwag in Python.
-https://github.com/rowanz/hellaswag
-
-Example HellaSwag json item:
-
-{"ind": 24, "activity_label": "Roof shingle removal", "ctx_a": "A man is sitting on a roof.", "ctx_b": "he", "ctx": "A man is sitting on a roof. he", "split": "val", "split_type": "indomain", "label": 3, "endings": ["is using wrap to wrap a pair of skis.", "is ripping level tiles off.", "is holding a rubik's cube.", "starts pulling up roofing on a roof."], "source_id": "activitynet~v_-JhWjGDPHMY"}
-
-ind: dataset ID
-activity_label: The ActivityNet or WikiHow label for this example
-context: There are two formats. The full context is in ctx. When the context ends in an (incomplete) noun phrase, like for ActivityNet, this incomplete noun phrase is in ctx_b, and the context up until then is in ctx_a. This can be useful for models such as BERT that need the last sentence to be complete. However, it's never required. If ctx_b is nonempty, then ctx is the same thing as ctx_a, followed by a space, then ctx_b.
-endings: a list of 4 endings. The correct index is given by label (0,1,2, or 3)
-split: train, val, or test.
-split_type: indomain if the activity label is seen during training, else zeroshot
-source_id: Which video or WikiHow article this example came from
-
-gpt2 (124M)
-- eleuther harness reports acc 28.92%, acc_norm 31.14% (multiple choice style)
-- this script: 10042 acc: 0.2859 acc_norm: 0.2955 (completion style)
-
-gpt2-xl (1558M)
-- eleuther harness reports acc 40.04%, acc_norm 50.89% (multiple choice style)
-- this script: 10042 acc: 0.3842 acc_norm: 0.4893 (completion style)
-
-The validation set of HellaSwag has a total of 10,042 examples.
 """
 
 import os
@@ -62,7 +38,7 @@ def download_file(url: str, fname: str, chunk_size=1024, split = None):
                 size = file.write(data)
                 bar.update(size)
     else:
-        hf_hub_download(repo_id="pauhidalgoo/hellaswag-val-ca", filename="hellaswag_val.jsonl", local_dir=fname)
+        hf_hub_download(repo_id="pauhidalgoo/hellaswag-val-ca", filename="hellaswag_val.jsonl", local_dir=fname) # We use our own translated Hellaswag.
 
 hellaswags = {
     "train": "https://raw.githubusercontent.com/rowanz/hellaswag/master/data/hellaswag_train.jsonl",
